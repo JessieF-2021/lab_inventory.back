e76333fe98641e16f1da11ac8e0bc2779f57126c
JessieF-2021 <jessieffianu@gmail.com>
2024-02-02 15:40:22 +0000

    laboratory backend

---
+++ routes/lab_routes.js
@@ -0,0 +1,14 @@
+
+import express from "express";
+import lab_controller from "../controller/lab_controller.js";
+// import { createLabItem } from "../controller/lab_controller.js";
+const labRouter = express.Router();
+
+// labRouter.post("/",createLabItem)
+labRouter.post("/", lab_controller.createLabItem);
+labRouter.get("/", lab_controller.fetchLabItems);
+labRouter.get("/:id", lab_controller.fetchLabItem);
+labRouter.put("/:id", lab_controller.updateLabItem);
+labRouter.delete("/:id", lab_controller.deleteLabItem);
+
+export default labRouter;
---
+++ server.js
@@ -0,0 +1,37 @@
+
+import express from 'express';
+
+import mongoose from 'mongoose';
+
+import morgan from 'morgan';
+
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import labRouter from './routes/lab_routes.js';
+dotenv.config();
+
+
+const app = express()
+const PORT = process.env.PORT || 6003;
+const URI = process.env.MONGO_URI;
+
+import cors from 'cors';
+
+
+// middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api", labRouter);
+
+
+//connecting to the db
+mongoose.connect(URI).then((results) => {
+    if(results) console.log('mongoDB connected')
+    }).catch((error) => console.log(error))
+    
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`)
+    })
